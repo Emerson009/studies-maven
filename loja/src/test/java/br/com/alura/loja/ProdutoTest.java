d512d58d918ce2b9dab4fc5f5ce2880e152a9807
Emerson009 <henriqueemerson023@gmail.com>
2023-01-16 09:40:10 -0300

    incluindo teste

--- loja/src/test/java/br/com/alura/loja/ProdutoTest.java
+++ loja/src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -1,11 +1,17 @@
 package br.com.alura.loja;
 
+import java.math.BigDecimal;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
+		Produto produto = new Produto("teste", BigDecimal.TEN);
+		Assert.assertEquals("teste", produto.getNome());
+		Assert.assertEquals(BigDecimal.TEN, produto.getPreco());
 	}
 
 }
